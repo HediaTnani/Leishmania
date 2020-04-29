ecf4347fbbb769485ee71db021a5f1a7bda6bc34
HediaTnani <59562743+HediaTnani@users.noreply.github.com>
2020-04-29 09:05:17 +0100

    Update probe2sym.R

--- probe2sym.R
+++ probe2sym.R
@@ -3,11 +3,14 @@ probe2sym <- function(sig, bmm, bmh) {
   m <- match(rownames(sig), genes$affy_mogene_1_0_st_v1)
   sig$gene <- genes[m, "mgi_symbol"]
   sig$entrez_id<- genes[m, "entrezgene_id"]
-  x=sig$entrez_id
-  genesV2 = getLDS(attributes = c("entrezgene_id"), filters = "entrezgene_id", values = x , mart = bmm, attributesL = c("hgnc_symbol"), martL = bmh)
+  sig$ensembl_mm<- genes[m, "ensembl_gene_id"]
+  x=sig$ensembl_mm
+  genesV2 = getLDS(attributes = c("ensembl_gene_id"), filters = "ensembl_gene_id", values = x , mart = bmm, attributesL = c("ensembl_gene_id"), martL = bmh)
   require(dplyr)
   require(readr)
   sig <- left_join(sig, genesV2, 
-                   by = c("entrez_id" = "NCBI.gene.ID"))
+                   by = c("ensembl_mm" = "Gene.stable.ID"))
+  sig$ensembl_hs=sig$Gene.stable.ID.1
+  sig=select(sig,-Gene.stable.ID.1)
   return(sig)
 }
