d7cab97b4a80e2eedbc61ed15b27673ae5832dad
HediaTnani <59562743+HediaTnani@users.noreply.github.com>
2020-05-06 08:12:43 +0100

    Update probe2sym.R

--- probe2sym.R
+++ probe2sym.R
@@ -1,5 +1,5 @@
 probe2sym <- function(sig, bmm, bmh) {
-  genes <- getBM(attributes = c("affy_mogene_1_0_st_v1", "mgi_symbol","entrezgene_id","ensembl_gene_id","hgnc_id"), filters = "affy_mogene_1_0_st_v1", values = rownames(sig), mart = bmm)
+  genes <- getBM(attributes = c("affy_mogene_1_0_st_v1", "mgi_symbol","entrezgene_id","ensembl_gene_id"), filters = "affy_mogene_1_0_st_v1", values = rownames(sig), mart = bmm)
   m <- match(rownames(sig), genes$affy_mogene_1_0_st_v1)
   sig$gene <- genes[m, "mgi_symbol"]
   sig$entrez_id<- genes[m, "entrezgene_id"]
