1a8439adb3533eee09caac2d8d3a3bffd38ad428
HediaTnani <59562743+HediaTnani@users.noreply.github.com>
2021-04-02 16:56:06 +0100

    Update probe2entrez.R

--- probe2entrez.R
+++ probe2entrez.R
@@ -2,7 +2,7 @@ probe2entrez <- function(sig, bm) {
   genes <- getBM(attributes = c("affy_mogene_1_0_st_v1", "entrezgene_id"), 
                  filters = "affy_mogene_1_0_st_v1", 
                  values = rownames(sig), 
-                 mart = bm)
+                 mart = bm,uniqueRows = TRUE)
   m <- match(rownames(sig), genes$affy_mogene_1_0_st_v1)
   sig$entrez_id <- genes[m, "entrezgene_id"]
   return(sig)
